e7c1e929cf8b6ab9f57d7c0d1d87e8cbeddb9a4c
Hayat Khan <hayatjmi007@gmail.com>
2022-11-25 22:42:21 +0530

    rewrite labour working hour code

--- 241122T2CP5.c
+++ 241122T2CP5.c
@@ -10,11 +10,39 @@
 // Name Hayat Khan
 // Mca Ist year
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
     int age;
     printf("Enter age :\n ");
-    scanf("%d",age);
+    scanf("%d",&age);
+    if(age<=0 && age>100)
+    {
+        printf("invalid input");
+
+    }
+    else if(age>0 && age <=10)
+    {
+        printf("0 hour ");
+    }
+    else if(age>10 && age <=15)
+    {
+        printf("0 hour ");
+    }
+    else if(age>15 && age <=20)
+    {
+        printf("3 hour ");
+    }
+    else if(age>20 && age <=25)
+    {
+        printf("6 hour ");
+    }
+    else 
+    {
+        printf("8 hour");
+    }
+    
+
     
 
 }
